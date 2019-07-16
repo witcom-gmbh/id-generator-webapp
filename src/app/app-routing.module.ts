e1e942135ee37c914fee5b9fb6a73439f99fa6f3
Carsten Buchberger <c.buchberger@witcom.de>
2019-07-16 08:11:51 +0200

    Moved Keycloak Authorization to external module (#1)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent} from './home/home.component';
+import { AuthztestComponent } from './authztest/authztest.component';
+import { AppAuthGuard } from './app-authguard';
 
 const routes: Routes = [
     {
       path: 'home', component: HomeComponent      
     },
+    /*{
+      path: 'authztest', component: AuthztestComponent,canActivate: [AppAuthGuard] ,data:{permissions:[{
+          rsname:"cf-service",
+          scope:"create"  
+      }]}
+    },*/
+    
     {
         path: '', redirectTo: '/home', pathMatch: 'full' 
     }
@@ -13,6 +22,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+providers: [AppAuthGuard]
 })
 export class AppRoutingModule { }
