fa8db8dc5b0d0447c5c5e0c8b6e5347de63935ec
Carsten Buchberger <c.buchberger@witcom.de>
2023-05-22 15:20:49 +0000

    fix: Provide default for API-Url
    If no API-URL provided we assume that the API is exposed
    with the same base-url as the web-app

--- src/app/config/api-init.ts
+++ src/app/config/api-init.ts
@@ -1,8 +1,14 @@
 import { ApiConfiguration } from '../api/api-configuration';
 import { ConfigService } from '../services/config.service';
+import t from 'typy';
 
 export function apiConfig(config: ApiConfiguration,configService:ConfigService): Function {
   return () => {
-    config.rootUrl = configService.apiConfig.url;
+    if(t(configService.apiConfig.url).isUndefined){
+      config.rootUrl = "";
+    } else {
+      config.rootUrl = configService.apiConfig.url;
+    }
+    
   };
 }
